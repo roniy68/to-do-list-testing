5d79638111d88580fcfc6a17b4c1b093efebae42
roniy68 <ahmed.11900449@lpu.in>
2023-01-26 13:33:02 +0530

    jest setup

---
+++ index.js
@@ -0,0 +1,1 @@
+export const addItem = () => console.log("add function")
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import { addItem } from "./index.js";
+
+test('Testing 001', () => {
+    expect(addItem).toBe('add function');
+})
