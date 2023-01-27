774ca60eaa9e1da359c3a6d8718862a31ef855ab
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-01-27 18:03:28 +0700

    Fix linter errs

--- src/toDoTasks.js
+++ src/toDoTasks.js
@@ -12,7 +12,9 @@ const remove = (taskIndex) => {
   const tasks = getTasks();
   const index = tasks.findIndex((item) => item.index === taskIndex);
   tasks.splice(index, 1);
-  tasks.forEach((item, i) => { item.index = i + 1 });
+  tasks.forEach((item, i) => { 
+    item.index = i + 1;
+  });
   saveTasks(tasks);
   return tasks;
 };
