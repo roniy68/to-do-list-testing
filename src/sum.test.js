d0429bfa93a33598737c8fe164f495238831d3d5
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-01-27 17:52:28 +0700

    Add testing for add and remove method

--- src/sum.test.js
+++
@@ -1,5 +0,0 @@
-import sum from './sum.js';
-
-test('2 + 3 is expected 5', () => {
-  expect(sum(2,3)).toBe(5);
-})
--- src/toDoTasks.js
+++ src/toDoTasks.js
@@ -2,7 +2,7 @@ import { getTasks, saveTasks } from './localStorage.js';
 
 const add = (task) => {
   let tasks = getTasks();
-  task.index = tasks.length + 1;
+  task.index = tasks.length;
   tasks.push(task);
   saveTasks(tasks);
   return tasks;
@@ -12,10 +12,12 @@ const remove = (taskIndex) => {
   const tasks = getTasks();
   const index = tasks.findIndex((item) => item.index === taskIndex);
   tasks.splice(index, 1);
-  for (let i = index; i < tasks.length; i += 1) {
-    tasks[i].index -= 1;
-  }
+  // for (let i = index; i < tasks.length; i += 1) {
+  //   tasks[i].index -= 1;
+  // }
+  tasks.forEach((item, i) => item.index = i + 1);
   saveTasks(tasks);
+  return tasks;
 };
 
 const modify = (description, index) => {
